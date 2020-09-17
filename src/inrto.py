020ea9f49d1210e18c9876d33b3af739b0887ca8
Kereell <kereell@gmail.com>
2020-09-17 12:15:34 +0300

    Created ini.py

--- src/inrto.py
+++ src/inrto.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import telegram
 from telegram.ext import Updater
+
 from config import TELEGRAM_KEREELLXBOT_TOKEN
 
-print(TELEGRAM_KEREELLXBOT_TOKEN)
+telegram_kereellx = telegram.Bot(
+    token=TELEGRAM_KEREELLXBOT_TOKEN
+)
+
+print(telegram_kereellx.get_me())
